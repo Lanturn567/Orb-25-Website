457d1632f76f7e041880e5de660335f213befcce
Andre Keh <andrekeh@gmail.com>
2025-09-16 21:32:47 +0800

    Add max score cap

--- trivia/models.py
+++ trivia/models.py
@@ -1,11 +1,14 @@
 # models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MaxValueValidator
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    max_score = models.IntegerField(default=0)
+    max_score = models.IntegerField(
+        default=0,
+        validators=[MaxValueValidator(1000000000000)]  # maximum score = 1000
+    )
     date_joined = models.DateTimeField(auto_now_add=True)
 
     # Add password validators
